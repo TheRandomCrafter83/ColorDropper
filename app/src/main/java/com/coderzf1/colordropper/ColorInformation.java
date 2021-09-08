2c9d52ff6285bcf2d2c105fa90cc6d411c31684a
TheRandomCrafter83 <williams.carl.83@gmail.com>
2021-09-08 19:18:36 -0400

    CommitTest

---
+++ app/src/main/java/com/coderzf1/colordropper/ColorInformation.java
@@ -0,0 +1,14 @@
+package com.coderzf1.colordropper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_information);
+    }
+}
---
+++ app/src/main/java/com/coderzf1/colordropper/FloatingWindow.java
@@ -0,0 +1,12 @@
+package com.coderzf1.colordropper;
+//test
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FloatingWindow extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.floating_window);
+    }
+}
